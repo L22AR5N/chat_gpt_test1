ca3732f7bca5c108e6b1439aa004dbb874f318fa
Aron <100554211+L22AR5N@users.noreply.github.com>
2023-04-07 10:36:39 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ st.header("🤖ARON's ChatGPT-3 (Demo)")
 
 st.markdown("[Be Original](https://github.com/L22AR5N/chat_gpt_test1/)")
 
-st.markdown(openai.api_key = os.getenv('API_KEY'))
+st.markdown("openai.api_key = os.getenv('API_KEY')")
  
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
